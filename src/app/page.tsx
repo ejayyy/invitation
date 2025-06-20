b6a02d9647b61724a70c6d8513311a7e1a27190a
ejayyy <ejay0811@gmail.com>
2025-06-20 10:38:00 +0900

    Update main title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
+    <div className="relative">
       <video className="w-screen h-screen object-cover" autoPlay loop muted playsInline poster="/thumbnail.png">
         <source src="/main.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="absolute top-[20%] left-0 right-0 p-4 text-center">
+        <h1 className="font-gowun-dodum text-6xl leading-1.2">10년의 끝에서<br />시작하는 하루</h1>
+      </div>
     </div>
   );
 }
