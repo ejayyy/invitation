6bd40db6f8331e84152c023d4b96372f978d9a96
ejayyy <ejay0811@gmail.com>
2025-06-20 09:46:14 +0900

    Update main video for safari

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <video className="w-screen h-screen object-cover" autoPlay loop muted poster="/thumbnail.png">
+      <video className="w-screen h-screen object-cover" autoPlay loop muted playsInline poster="/thumbnail.png">
         <source src="/main.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
