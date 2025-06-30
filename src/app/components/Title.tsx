74b31840e59bbdffff1ae8385dd087462f8abbb7
ejayyy <ejay0811@gmail.com>
2025-06-30 14:04:02 +0900

    Change layout

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -3,21 +3,21 @@ import { WEDDING_CONFIG } from "../config/wedding";
 
 const Title = () => {
   return (
-    <section className="overflow-hidden">
-      <div className="w-full text-center pt-10 font-medium animate-fade-in">
-        <p className="text-sm opacity-45 mb-4">10년의 끝에서 시작하는 하루</p>
-        <p className="text-2xl font-bold opacity-90 mb-4">
-          {WEDDING_CONFIG.groom.name} & {WEDDING_CONFIG.bride.name}
-        </p>
-        <p className="text-base opacity-65 mb-6">
+    <section className="-mx-3 relative mt-0 min-h-screen overflow-hidden">
+      <video className="absolute inset-0 h-full w-full object-cover" autoPlay loop muted playsInline poster="/thumbnails/thumbnail.png">
+        <source src="/assets/BackgroundVideo.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute inset-x-0 top-[18%] sm:top-[10%] font-medium animate-fade-in text-center text-white">
+        <p className="text-base">10년의 끝에서 시작하는 하루</p>
+        <div className="mt-3 mb-5">
+          <p className="text-3xl font-bold">{WEDDING_CONFIG.groom.name} & {WEDDING_CONFIG.bride.name}</p>
+        </div>
+        <p className="text-md">
           {WEDDING_CONFIG.date}
           <br />
           {WEDDING_CONFIG.location}
         </p>
       </div>
-      <video className="w-full" autoPlay loop muted playsInline>
-        <source src="/assets/BackgroundVideo.mp4" type="video/mp4" />
-      </video>
     </section>
   );
 };
